c7303f9a03226471d97a6b56fa22557f078a9ef5
Brady Menswar <bradymenswar@gmail.com>
2025-02-11 19:28:11 -0500

    add sshagent to startup

--- configuration.nix
+++ configuration.nix
@@ -116,6 +116,8 @@
     discord
   ];
 
+  programs.ssh.startAgent = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
