1d6e0f8e0c26b4e97f1baa164a6ca4608936d405
Brady Menswar <bradymenswar@gmail.com>
2025-02-13 07:12:16 -0500

    setup font in stylix

--- home.nix
+++ home.nix
@@ -16,11 +16,36 @@
     image = ./home/wallpapers/wallpaper.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
     polarity = "dark";
+    opacity = {
+      terminal = 0.9;
+    };
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
+
+  home.packages = with pkgs; [
+    pciutils
+  ];
 
   programs.home-manager.enable = true;
 }
